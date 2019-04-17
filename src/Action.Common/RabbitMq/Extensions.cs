d2b65342307d71324f18dd20d4fe3541fa5861e1
Murad <muradheyderov@gmail.com>
2019-04-17 15:25:54 +0400

    subscribe services added

---
+++ src/Action.Common/RabbitMq/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using Action.Common.Commands;
+using RawRabbit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Action.Common.Events;
+
+namespace Action.Common.RabbitMq
+{
+    public static class Extensions
+    {
+        public static Task WithCommandHandlerAsync<TCommand>(this IBusClient busClient, 
+            ICommandHandler<TCommand> handler) where TCommand : ICommand
+        {
+            return busClient.SubscribeAsync<TCommand>(msg => handler.HandleAsync(msg),
+                ctx => ctx.UseSubscribeConfiguration(
+                    cfg => cfg.FromDeclaredQueue(
+                        q => q.WithName(GetQueueName<TCommand>()))));
+        }
+
+        public static Task WithEventHandlerAsync<TEvent>(this IBusClient busClient,
+            IEventHandler<TEvent> handler) where TEvent : IEvent
+        {
+            return busClient.SubscribeAsync<TEvent>(msg => handler.HandleAsync(msg),
+                ctx => ctx.UseSubscribeConfiguration(
+                    cfg => cfg.FromDeclaredQueue(
+                        q => q.WithName(GetQueueName<TEvent>()))));
+        }
+
+        private static string GetQueueName<T>()
+            => $"{Assembly.GetEntryAssembly().GetName()}/{typeof(T).Name}";
+    }
+}
